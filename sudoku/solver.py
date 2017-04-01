230e3fedbeeddd8f5c5ed0aac84840e506f004bd
Helio <heliohenriqHelioue@ufpr.br>
2017-04-01 13:28:35 -0300

    Continuando Sudoku.

--- sudoku/solver.py
+++ sudoku/solver.py
@@ -4,32 +4,54 @@ from sudoku.dados import *
 import time
 import bisect
 import random
+from Queue import Queue
+import threading
 
-TENTATIVAS = 100
+TENTATIVAS = 200000
 
-class FilaPrioridadeLimitada:
+class FilaPrioridadeLimitada():
     
-    def __init__(self, maxlen=-1):
-        self._queue = []
-        self._maxlen = maxlen
+    def __init__(self, maxsize):
+        self.queue = []
+        self.mutex = threading.Lock()
+        self.maxsize = maxsize
+        self.len = 0
+
+
+    def put(self, item):
+        self.mutex.acquire()
+        self.queue = self.queue[:-1]
         
-    def get(self,i):
-        return self._queue[i]
+                    #         if len(self.queue) == self.maxsize:
+        #             last = self._queue.pop()
+        # #             if last < item:
+        # #                 self.queue.append(last)
+        # #                 return
         
-    def pop(self):
-        return self._queue.pop(0)
+        bisect.insort(self.queue, item)
+        self.len+=1
+        self.mutex.release()
+        
     
-    def put(self,item):
-        if len(self._queue) == self._maxlen:
-            last = self._queue.pop()
-            if last < item:
-                self._queue.append(last)
-                return
-        
-        bisect.insort(self._queue, item)
+    def get(self):
+        self.mutex.acquire()
+        item = self.queue.pop(0)
+        self.len-=1
+        self.mutex.release()
+        return item
+    
+    
+   
+    def getAll(self):
+        return [item for item in self.queue]
+
             
     def last(self):
-        return self._queue[len(self._queue)-1]       
+        self.mutex.acquire()
+        last = self.queue[self.len-1]
+        self.mutex.release()
+        return last
+    
     
     
 
@@ -48,9 +70,10 @@ class Solver:
                 if (lin,col) not in tab.preenchidos:
                     tab.flip(lin,col)
                     fitness = tab.countInvalidos()
-                    if fitness < tab.getFitness() and fitness < self.vizinhos.last().getFitness():                       
+                    if fitness < self.vizinhos.last().getFitness():                       
                         novoTab = tab.clone()
                         novoTab.setFitness(fitness)
+                        self.vizinhos.removeLast()
                         self.vizinhos.put(novoTab)
                     tab.unflip(lin,col)
 
@@ -64,11 +87,33 @@ class Solver:
 
             tab.flip(lin,col)
             fitness = tab.countInvalidos()
-            if fitness < tab.getFitness() and fitness < self.vizinhos.last().getFitness():                       
+            if fitness < self.vizinhos.last().getFitness():                       
                 novoTab = tab.clone()
                 novoTab.setFitness(fitness)
                 self.vizinhos.put(novoTab)
             tab.unflip(lin,col)
+            
+            
+    def proximos_vizinhos_flip_total_random(self,tab,vizinhosAtuais):     
+            
+        #for i in range(9):
+        lin,col = (random.randrange(0,9),random.randrange(0,9))
+        while (lin,col) in tab.preenchidos:
+            col = random.randrange(0,9)
+
+        randomnum = random.randrange(1,10)
+        anterior = tab[lin][col]
+        tab[lin][col] = randomnum
+        fitness = tab.countInvalidos()
+        try:
+            if fitness < vizinhosAtuais[-1].getFitness():                       
+                novoTab = tab.clone()
+                novoTab.setFitness(fitness)
+                self.vizinhos.put(novoTab)
+        #except IndexError: print("erro")
+        finally:
+            tab[lin][col] = anterior
+
 
     def resolver_sudoku(self, metodoVizinhos):
         for i in range(self.k):
@@ -78,17 +123,17 @@ class Solver:
         tentativas = 0
         while True:
             
-            if self.vizinhos.get(0).estahResolvido() or tentativas == TENTATIVAS:
-                return self.vizinhos.pop()
+            melhor = self.vizinhos.get()
+            if melhor.estahResolvido() or tentativas == TENTATIVAS:
+                return melhor
+            self.vizinhos.put(melhor)
             
-            vizinhosAtuais = []
-            
-            for i in range(self.k):                
-                v = self.vizinhos.get(i)
-                vizinhosAtuais.append(v)
+            vizinhosAtuais = self.vizinhos.getAll()
 
             for v in vizinhosAtuais:
-                metodoVizinhos(v)
+                t = threading.Thread(target=self.proximos_vizinhos_flip_total_random, kwargs = {"tab":v, "vizinhosAtuais":vizinhosAtuais} )
+                t.daemon = True
+                t.start()
             
             tentativas += 1
             print(tentativas)
@@ -107,7 +152,7 @@ class Solver:
 # t_final = time.time()
 # print "Tempo de execução =", t_final - t_inicial
 
-solver = Solver(TAB_TAREFA,100)
+solver = Solver(TAB_TAREFA,1000)
 solucao = solver.resolver_sudoku(solver.proximos_vizinhos_flip_um_por_linha_random)
 solucao.printthis()
 print(solucao.fitness)
